d7d0178add9a907c927e4af973f0c8ff0aa5524c
Brandon-Lyons <brandon.joseph.lyons@gmail.com>
2013-08-04 17:57:04 -0500

    added number of days method

--- lib/month.py
+++ lib/month.py
@@ -16,10 +16,21 @@ class Month(object):
 		m = self.month
 		y = self.year
 		if self.month == 1 or self.month == 2:
-		  m = self.month + 12
-		  y = self.year - 1
+			m = self.month + 12
+			y = self.year - 1
 		first_day = ((1 + (((m + 1) * 26) / 10) + y + (y / 4) + 6 * (y / 100) + (y / 400)) % 7) - 1
 		if first_day == -1:
 			first_day = 6
 		return first_day
 
+	def days_number(self):
+		thirty = [4,6,9,11]
+		thirtyone = [1,3,5,7,8,10,12]
+		if self.month in thirty:
+			return 30
+		elif self.month in thirtyone:
+			return 31
+		elif self.year % 400 == 0 or (self.year % 4 == 0 and self.year % 100 != 0):
+			return 29
+		else:
+			return 28
--- test/test_cal_month.py
+++ test/test_cal_month.py
@@ -3,29 +3,49 @@ from month import Month
 
 class MonthUnitTests(unittest.TestCase):
 
-  def test_header(self):
-    cal = Month(5, 2012)
-    result = cal.header()
-    self.assertEqual("      May 2012", result)
+	def test_header(self):
+		cal = Month(5, 2012)
+		result = cal.header()
+		self.assertEqual("      May 2012", result)
 
-  def test_header_different_month(self):
-  	cal = Month(3, 2012)
-  	result = cal.header()
-  	self.assertEqual("     March 2012", result)
+	def test_header_different_month(self):
+		cal = Month(3, 2012)
+		result = cal.header()
+		self.assertEqual("     March 2012", result)
 
-  def test_zeller(self):
-  	cal = Month(3, 1995)
-  	result = cal.zeller()
-  	self.assertEqual(3, result)
+	def test_zeller(self):
+		cal = Month(3, 1995)
+		result = cal.zeller()
+		self.assertEqual(3, result)
 
-  def test_zeller_again(self):
-    cal = Month(6, 2999)
-    self.assertEqual(6, cal.zeller())
+	def test_zeller_again(self):
+		cal = Month(6, 2999)
+		self.assertEqual(6, cal.zeller())
 
-  def test_zeller_january(self):
-    cal = Month(1, 2000)
-    self.assertEqual(6, cal.zeller())
+	def test_zeller_january(self):
+		cal = Month(1, 2000)
+		self.assertEqual(6, cal.zeller())
 
-  def test_zeller_february(self):
-    cal = Month(2, 2000)
-    self.assertEqual(2, cal.zeller())
+	def test_zeller_february(self):
+		cal = Month(2, 2000)
+		self.assertEqual(2, cal.zeller())
+
+	def test_number_of_days(self):
+		cal = Month(6, 1900)
+		self.assertEqual(30, cal.days_number())
+
+	def test_number_of_days_february(self):
+		cal = Month(2, 1995)
+		self.assertEqual(28, cal.days_number())
+
+	def test_number_of_days_leap_year(self):
+		cal = Month(2, 1996)
+		self.assertEqual(29, cal.days_number())
+
+	def test_number_of_days_leap_century(self):
+		cal = Month(2, 2000)
+		self.assertEqual(29, cal.days_number())
+
+	def test_number_of_days_non_leap_century(self):
+		cal = Month(2, 1900)
+		self.assertEqual(28, cal.days_number())
