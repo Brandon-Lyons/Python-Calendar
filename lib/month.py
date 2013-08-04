eedbf8cc5bf50f75e7dedc1dccc8c516ec5bfc72
Brandon-Lyons <brandon.joseph.lyons@gmail.com>
2013-08-04 17:44:05 -0500

    added zeller's congruence

--- lib/month.py
+++ lib/month.py
@@ -1,6 +1,25 @@
 class Month(object):
 
-	def header(self, month, year):
+	def __init__(self, month, year):
+		self.month = month
+		self.year = year
+
+	def header(self):
 		month_names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-		string = "%s %s" % (month_names[month -1], year)
+		string = "%s %s" % (month_names[self.month -1], self.year)
 		return string.center(20, ' ').rstrip()
+
+	def week_row(self):
+		return "Su Mo Tu We Th Fr Sa"
+
+	def zeller(self):
+		m = self.month
+		y = self.year
+		if self.month == 1 or self.month == 2:
+		  m = self.month + 12
+		  y = self.year - 1
+		first_day = ((1 + (((m + 1) * 26) / 10) + y + (y / 4) + 6 * (y / 100) + (y / 400)) % 7) - 1
+		if first_day == -1:
+			first_day = 6
+		return first_day
+
--- test/test_cal_month.py
+++ test/test_cal_month.py
@@ -4,11 +4,28 @@ from month import Month
 class MonthUnitTests(unittest.TestCase):
 
   def test_header(self):
-    cal = Month()
-    result = cal.header(5, 2012)
+    cal = Month(5, 2012)
+    result = cal.header()
     self.assertEqual("      May 2012", result)
 
   def test_header_different_month(self):
-  	cal = Month()
-  	result = cal.header(3, 2012)
+  	cal = Month(3, 2012)
+  	result = cal.header()
   	self.assertEqual("     March 2012", result)
+
+  def test_zeller(self):
+  	cal = Month(3, 1995)
+  	result = cal.zeller()
+  	self.assertEqual(3, result)
+
+  def test_zeller_again(self):
+    cal = Month(6, 2999)
+    self.assertEqual(6, cal.zeller())
+
+  def test_zeller_january(self):
+    cal = Month(1, 2000)
+    self.assertEqual(6, cal.zeller())
+
+  def test_zeller_february(self):
+    cal = Month(2, 2000)
+    self.assertEqual(2, cal.zeller())
